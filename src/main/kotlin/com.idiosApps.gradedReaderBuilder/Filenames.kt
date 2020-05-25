383b23872b5644002eab7c4c9ba89c2e3ea75f79
James <IdiosApps@users.noreply.github.com>
2020-05-25 18:03:16 +0100

    [Filenames] Remove local filenames as POST provides

--- src/main/kotlin/com.idiosApps.gradedReaderBuilder/Filenames.kt
+++ src/main/kotlin/com.idiosApps.gradedReaderBuilder/Filenames.kt
@@ -1,42 +1,10 @@
 package com.idiosApps.gradedReaderBuilder;
 
-import java.io.File
-
 class Filenames {
     companion object {
-        private const val inputPrefix: String = "./input/"
         private const val outputPrefix: String = "./output/"
-
-        val inputHeaderResource = this::class.java.classLoader.getResourceAsStream("org.idiosapps/texHeader")
-
-        const val inputTitleFilename: String = inputPrefix + "title"
-        const val inputStoryFilename: String = inputPrefix + "story"
-
-        const val inputVocabFilename: String = inputPrefix + "vocab"
-        const val inputKeyNamesFilename: String = inputPrefix + "names"
 
         const val outputTexFilename: String = outputPrefix + "outputStory.tex"
         const val outputPDFFilename: String = outputPrefix + "outputStory.pdf"
-
-        fun checkInputs() {
-            val inputArray = arrayListOf(
-                inputTitleFilename,
-                inputStoryFilename,
-                inputVocabFilename,
-                inputKeyNamesFilename
-            )
-
-            for (input in inputArray) {
-                val inputFile = File(input)
-                if (!inputFile.exists()) { // could also check for size > 0 on required inputs
-                    throw Exception("Input does not exist: $input")
-                }
-            }
-        }
-
-        fun hasContent(filename: String): Boolean {
-            val inputFile = File(filename)
-            return inputFile.length() > 0
-        }
     }
 }
