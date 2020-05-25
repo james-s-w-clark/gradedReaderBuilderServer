3c49b15c41a14e3897c80b8c5497521c36f1d20b
james <idiosapps@gmail.com>
2020-05-26 00:47:18 +0100

    [BuildController] Return files in HttpServletResponse

--- src/main/java/com/idiosApps/gradedReaderBuilderServer/BuildController.java
+++ src/main/java/com/idiosApps/gradedReaderBuilderServer/BuildController.java
@@ -1,11 +1,13 @@
 package com.idiosApps.gradedReaderBuilderServer;
 
 import com.idiosApps.gradedReaderBuilder.BuilderPipeline;
+import org.apache.pdfbox.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 
@@ -13,11 +15,12 @@ import java.util.List;
 public class BuildController {
 
     @RequestMapping(value = "/build", method = RequestMethod.POST)
-    public File build(@RequestParam(name="outputType", required=true) String outputType,
+    public void build(@RequestParam(name="outputType", required=true) String outputType,
                       @RequestPart(name="titleFile", required=true) MultipartFile titleUpload,
                       @RequestPart(name="storyFile", required=true) MultipartFile storyUpload,
                       @RequestPart(name="vocabFile", required=true) MultipartFile vocabUpload,
-                      @RequestPart(name="namesFile", required=true) MultipartFile namesUpload) throws IOException {
+                      @RequestPart(name="namesFile", required=true) MultipartFile namesUpload,
+                      HttpServletResponse response) throws IOException {
 
         try (TemporaryDirectory tempDirectory = new TemporaryDirectory()) {
             BuilderPipeline pipeline = new BuilderPipeline(
@@ -30,10 +33,22 @@ public class BuildController {
             TemporaryFile pdfFile = tempDirectory.getFile(".pdf");
             pipeline.buildGradedReader(texFile, pdfFile);
 
-            if ("pdf".equals(outputType))
-                return pdfFile;
-            TemporaryFile zip = tempDirectory.getFile(".zip");
-            return ZipUtils.zipFiles(zip, List.of(pdfFile, texFile));
+            TemporaryFile outFile;
+            String contentType;
+            if ("pdf".equals(outputType)) {
+                outFile = pdfFile;
+                contentType = "application/pdf";
+            }
+            else {
+                outFile = tempDirectory.getFile(".zip");
+                ZipUtils.zipFiles(outFile, List.of(pdfFile, texFile));
+                contentType = "application/zip";
+            }
+
+            IOUtils.copy(new FileInputStream(outFile), response.getOutputStream());
+            response.flushBuffer();
+            response.setContentType(contentType);
+            return;
         }
     }
 }
