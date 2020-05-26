ca667d7903b747fafff6f1b25c8695394cf6004b
james <idiosapps@gmail.com>
2020-05-26 08:58:54 +0100

    [Application] write spring.log to app working directory

--- src/main/java/com/idiosApps/gradedReaderBuilderServer/Application.java
+++ src/main/java/com/idiosApps/gradedReaderBuilderServer/Application.java
@@ -2,6 +2,8 @@ package com.idiosApps.gradedReaderBuilderServer;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,23 +12,24 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+    private static Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        LOGGER.info("App is running at " + System.getProperty("user.dir") + ". Logs will be saved here");
     }
 
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
+//            System.out.println("Let's inspect the beans provided by Spring Boot:");
 
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
             for (String beanName : beanNames) {
-                System.out.println(beanName);
+//                System.out.println(beanName);
             }
-
         };
     }
 
